473a6aeede69d94fe8cbbd5558eae3f02b702fde
slyslyme <slyslyme@163.com>
2019-05-19 10:21:01 +0800

    notice, choiceLibrary and info, about their adminx and viewset

--- apps/choice/serializers.py
+++ apps/choice/serializers.py
@@ -6,4 +6,4 @@ class ChoiceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ChoiceLibrary
-        fields = ('choice_question','choice_a','choice_b','choice_c','choice_d')
+        fields = ('choice_question', 'choice_a', 'choice_b', 'choice_c','choice_d', 'choice_score')
