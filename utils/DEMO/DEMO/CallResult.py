3e41d17ea0c2835a5ec714994d4cc0faf18733db
xuchaoa <xuchaoao@outlook.com>
2019-05-20 10:24:35 +0800

    5.19 添加DEMO

---
+++ utils/DEMO/DEMO/CallResult.py
@@ -0,0 +1,49 @@
+#coding=gbk
+
+#coding=utf-8
+
+#-*- coding: UTF-8 -*-  
+
+from CCPRestSDK import REST
+import ConfigParser
+
+#主帐号
+accountSid= '您的主帐号';
+
+#主帐号Token
+accountToken= '您的主帐号Token';
+
+#应用Id
+appId='您的应用ID';
+
+#请求地址，格式如下，不需要写http://
+serverIP='app.cloopen.com';
+
+#请求端口 
+serverPort='8883';
+
+#REST版本号
+softVersion='2013-12-26';
+
+    # 呼叫结果查询
+    # @param callsid   必选参数    呼叫ID
+    
+def CallResult(callsid):
+    
+    #初始化REST SDK
+    rest = REST(serverIP,serverPort,softVersion)
+    rest.setAccount(accountSid,accountToken)
+    rest.setAppId(appId)
+    
+    result = rest.CallResult(callsid)
+
+    for k,v in result.iteritems(): 
+        
+        if k=='CallResult' :
+                for k,s in v.iteritems(): 
+                    print '%s:%s' % (k, s)
+        else:
+            print '%s:%s' % (k, v)
+   
+   
+#CallResult('呼叫ID')
